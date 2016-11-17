0a9eb403d5083c779b05c678bd07233bf2fef6c4
maurobai <maurobai@hotmail.com>
2016-11-17 17:10:28 +0100

    Uploading version of 17 november.

--- template_fichiers/chapitre.php
+++ template_fichiers/chapitre.php
@@ -1,19 +1,20 @@
 <?php
-	session_start();
-	include 'config.php';
-	include 'lib_fonctions.php';
-	if(!isset($_SESSION['user'])){
-        	echo '<script language="javascript">document.location.replace("index.php");</script>'; 
-	}
+session_start();
+include 'config.php';
+include 'lib_fonctions.php';
+if(!isset($_SESSION['user'])){
+	echo '<script language="javascript">document.location.replace("index.php");</script>';
+}
 ?>
 <legend>Saisie d'un chapitre dans un ouvrage général</legend>        
                 <div id="chercherouvrage">
-                <p>Vous allez saisir un nouveau chapitre d'ouvrage ou une préface, postface, introduction dans un ouvrage
-                    <label class="control-label col-sm-8">Rechercher le titre de l'ouvrage</label> ou <a onClick="nouvel_ouvrage();">proposer un ouvrage collectif</a>
+                <p>Vous allez saisir un nouveau chapitre d'ouvrage ou une préface, postface, introduction dans un ouvrage.<br><br>
+                    <label class="control-label col-sm-8">Rechercher le titre de l'ouvrage</label>
                         <input list="TitreOuvrage" name="TitreOuvrage" size="55" maxlength="255" required>
                         <datalist id='TitreOuvrage'>
-                        <?php afficherTousLesOuvragesFormOption(); ?>
+                        	<?php afficherTousLesOuvragesFormOption(); ?>
                         </datalist>
+                   	<br>ou <a onClick="nouvel_ouvrage();" style="color: #1f398f;">proposer un ouvrage collectif</a>
                 </p>
                 </div>
                         <p>
